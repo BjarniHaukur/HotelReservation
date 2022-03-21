c6e15b2750793998bbfb71346f36f775dd2f576e
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-03-21 22:09:24 +0000

    asdfasdf

--- test/hotelreservation/hotelControllerTest.java
+++ test/hotelreservation/hotelControllerTest.java
@@ -1,5 +1,31 @@
-package test;
+package hotelreservation;
+
+import hotelreservation.entities.*
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class hotelControllerTest {
-    
+
+    private Hotel hotel1;
+    // private HotelController hotelController;
+    @Before
+    public void setUp(){
+        hotel1 = new Hotel(1,2,3,4);
+        hotelController = HotelController.getInstance();
+    }
+
+    @After
+    public void tearDown(){
+        hotel1 = null;
+    }
+
+    @test
+    public void testID(){
+        assertEquals(1,hotel1.getID());
+    }
+
 }
