9277a96bd8b3f15b2618aad9f5dacb1a8beba66b
Bobbi11 <bjarnihaukur11@gmail.com>
2022-03-21 22:09:14 +0000

    Getters setters

--- src/hotelreservation/entities/Hotel.java
+++ src/hotelreservation/entities/Hotel.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 public class Hotel {
 
 
-    
-
     private String id;
     private String name;
     private int starRating;
@@ -25,4 +23,40 @@ public class Hotel {
     public void addRoom(Room newRoom) {
         rooms.add(newRoom);
     }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStarRating() {
+        return this.starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public Price getPriceRating() {
+        return this.priceRating;
+    }
+
+    public void setPriceRating(Price priceRating) {
+        this.priceRating = priceRating;
+    }
+
+    public ArrayList<Room> getRooms() {return this.rooms;}
+
+    public void setRooms(ArrayList<Room> rooms) {this.rooms = rooms;}
 }
--- src/hotelreservation/main.java
+++ src/hotelreservation/main.java
@@ -1,12 +1,12 @@
 package hotelreservation;
 import java.util.ArrayList;
 import hotelreservation.entities.*;
-import hotelreservation.entities.Enums.*;
+
 
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         ArrayList<Room> rooms = new ArrayList<Room>();
-        Hotel ranga = new Hotel("a", "Rangá", 5, Price.THIRD, rooms);
+        Hotel ranga = new Hotel("a", "Rangá", 5, Enums.Price.THIRD, rooms);
     }
 }
