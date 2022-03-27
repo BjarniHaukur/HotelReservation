f7618b87c3d61da2f092bd76432c4de07b9a94a8
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-03-27 21:58:23 +0000

    v4

--- src/hotelreservation/entities/HotelTest.java
+++ src/hotelreservation/entities/HotelTest.java
@@ -4,22 +4,29 @@ import hotelreservation.controllers.*;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
 
 public class HotelTest {
     private HotelControllerMock hotelController;
 
     @Before
-    void setUp() {
+    public void setUp() {
         hotelController = HotelControllerMock.getInstance();
     }
 
+    @After
+    public void tearDown() {
+        hotelController = null;
+    }
+
     // Þarf þetta?
-    // @Test
-    // public void testGetId() {
-    //     assertEquals("Rangá", hotelController.getHotelByID(1).getName());
-    //     assertEquals("Hilton", hotelController.getHotelByID(2).getName());
-    //     assertEquals("Ölfus", hotelController.getHotelByID(3).getName());
-    // }
+    @Test
+    public void testGetId() {
+        assertEquals((Integer) 1, hotelController.getHotelByID(1).getId());
+        assertEquals((Integer) 2, hotelController.getHotelByID(2).getId());
+        assertEquals((Integer) 3, hotelController.getHotelByID(3).getId());
+    }
 
     @Test
     public void testGetName() {
@@ -29,14 +36,10 @@ public class HotelTest {
     }
 
     @Test
-    public void testGetPriceRating(Integer ID) {
-        assertEquals(3, hotelController.getHotelByID(1).getName());
-        assertEquals(4, hotelController.getHotelByID(2).getName());
-        assertEquals(5, hotelController.getHotelByID(3).getName());
+    public void testGetPriceRating() {
+        assertEquals((Integer) 3, hotelController.getHotelByID(1).getPriceRating());
+        assertEquals((Integer) 4, hotelController.getHotelByID(2).getPriceRating());
+        assertEquals((Integer) 5, hotelController.getHotelByID(3).getPriceRating());
     }
 
-    @After
-    public void tearDown(){
-        hotelController = null;
-    }
 }
