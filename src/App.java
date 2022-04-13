6efebf13243b0586515b374449047d2c0416d9b5
Bobbi11 <bjarnihaukur11@gmail.com>
2022-04-13 14:24:44 +0000

    db virkar

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 import java.sql.SQLException;
 import hotelreservation.controllers.HotelController;
 import hotelreservation.data.HotelDataConnection;
+import java.util.ArrayList;
 
 public class App {
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         HotelDataConnection connection = HotelDataConnection.getInstance();
         
+        ArrayList<Hotel>
     }
 }
--- src/hotelreservation/data/HotelDataConnection.java
+++ src/hotelreservation/data/HotelDataConnection.java
@@ -57,44 +57,44 @@ public class HotelDataConnection {
 
     private void initializeDatabaseFromScript() throws Exception {
         InputStream scriptStream = null;
-            // ApplicationDirectory returns the private read-write sandbox area
-            // of the mobile device's file system that this application can access.
-            // This is where the database is created
-            String dbName = "/HotelData.db";
+        // ApplicationDirectory returns the private read-write sandbox area
+        // of the mobile device's file system that this application can access.
+        // This is where the database is created
+        String dbName = "/HotelData.db";
 
-            // Verify whether or not the database exists.
-            // If it does, then it has already been initialized
-            // and no further actions are required
-            File dbFile = new File(dbName);
-            if (dbFile.exists())
-                return;
+        // Verify whether or not the database exists.
+        // If it does, then it has already been initialized
+        // and no further actions are required
+        File dbFile = new File(dbName);
+        if (dbFile.exists())
+            return;
 
-            String current = System.getProperty("user.dir");
-            // Since the SQL script has been packaged as a resource within
-            // the application, the getResourceAsStream method is used
-            scriptStream = Thread.currentThread().getContextClassLoader().
-                    getResourceAsStream("META-INF/initialize.sql");
-            BufferedReader scriptReader = new BufferedReader(new FileReader(current + "/hotelreservation/data/database.sql"));
-            String nextLine;
-            StringBuffer nextStatement = new StringBuffer();
+        String current = System.getProperty("user.dir");
+        // Since the SQL script has been packaged as a resource within
+        // the application, the getResourceAsStream method is used
+        scriptStream = Thread.currentThread().getContextClassLoader().
+                getResourceAsStream("META-INF/initialize.sql");
+        BufferedReader scriptReader = new BufferedReader(new FileReader(current + "/hotelreservation/data/database.sql"));
+        String nextLine;
+        StringBuffer nextStatement = new StringBuffer();
 
-            // The while loop iterates over all the lines in the SQL script,
-            // assembling them into valid SQL statements and executing them as
-            // a terminating semicolon is encountered
-            Statement stmt = conn.createStatement();
-            while ((nextLine = scriptReader.readLine()) != null) {
-                // Skipping blank lines, comments, and COMMIT statements
-                if (nextLine.startsWith("REM") ||
-                        nextLine.startsWith("COMMIT") ||
-                        nextLine.length() < 1)
-                    continue;
-                nextStatement.append(nextLine);
-                if (nextLine.endsWith(";")) {
-                    stmt.execute(nextStatement.toString());
-                    nextStatement = new StringBuffer();
-                }
+        // The while loop iterates over all the lines in the SQL script,
+        // assembling them into valid SQL statements and executing them as
+        // a terminating semicolon is encountered
+        Statement stmt = conn.createStatement();
+        while ((nextLine = scriptReader.readLine()) != null) {
+            // Skipping blank lines, comments, and COMMIT statements
+            if (nextLine.startsWith("REM") ||
+                    nextLine.startsWith("COMMIT") ||
+                    nextLine.length() < 1)
+                continue;
+            nextStatement.append(nextLine);
+            if (nextLine.endsWith(";")) {
+                stmt.execute(nextStatement.toString());
+                nextStatement = new StringBuffer();
             }
-			scriptReader.close();
+        }
+        scriptReader.close();
     }
 
     public ArrayList<Hotel> getAllHotels() throws Exception{
